f5530b29b6edcda5540ac9b6b33f67e59792db4c
hiroshi.nishigami@access-company.com <Hiroshi.Nishigami@Y55-66745.local>
2019-05-16 16:48:30 +0900

    show.ex の IO.inspect をコメントアウトしました。  IO.inspect があると antikythera のdeploy が失敗するため。

--- web/controller/question/show.ex
+++ web/controller/question/show.ex
@@ -6,12 +6,12 @@ defmodule StackoverflowCloneL.Controller.Question.Show do
 
   def show(%Antikythera.Conn{request: %Antikythera.Request{path_matches: %{id: my_id}}} = conn) do
     # Implement me
-    IO.inspect(conn)
-    IO.inspect(my_id)
+    #IO.inspect(conn)
+    #IO.inspect(my_id)
 
     req = Dodai.RetrieveDedicatedDataEntityRequest.new(SD.default_group_id(),"Question",my_id,SD.root_key())
     res = Sazabi.G2gClient.send(conn.context, SD.app_id(), req)
-    IO.inspect(res)
+    #IO.inspect(res)
     Conn.json(conn, 200, %{"hello" => res})
     case res do
       %Dodai.RetrieveDedicatedDataEntitySuccess{body: doc} -> Conn.json(conn, 200, Helper.to_response_body(doc))
