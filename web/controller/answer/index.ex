40e5927e8375fefb80d0efd43e43504aa649f600
aTakatoNakamura <takato.nakamura@access-company.com>
2019-05-20 13:00:26 +0900

    [add] request parameterとしてuser_idまたはquestion_idが指定された場合は、その指定された値をもつanswerのみを返すように拡張

--- web/controller/answer/index.ex
+++ web/controller/answer/index.ex
@@ -1,16 +1,40 @@
+use Croma
+
+defmodule StackoverflowCloneL.Controller.Answer.IndexRequestParams do
+  use Croma.Struct, fields: [
+    user_id:  Croma.TypeGen.nilable(Croma.String),
+    question_id: Croma.TypeGen.nilable(Croma.String),
+  ]
+end
+
 defmodule StackoverflowCloneL.Controller.Answer.Index do
   use StackoverflowCloneL.Controller.Application
+  alias Sazabi.G2gClient
   alias StackoverflowCloneL.Dodai, as: SD
-  alias StackoverflowCloneL.Controller.Answer.Helper
+  alias StackoverflowCloneL.Controller.Answer.{Helper, IndexRequestParams}
+  alias StackoverflowCloneL.Error.BadRequestError
 
-  def index(conn) do
+  def index(%Conn{request: %Request{query_params: query_params}, context: context} = conn) do
     # Implement me
-    query = %Dodai.RetrieveDedicatedDataEntityListRequestQuery{
-      query: %{},
-      sort:  %{"_id" => 1},
+    case IndexRequestParams.new(query_params) do
+      {:errir, _}      ->
+        ErrorJson.json_by_error(conn,BadRequestError.new())
+      {:ok, validated} ->
+        query = convert_to_dodai_req_query(validated)
+        req = Dodai.RetrieveDedicatedDataEntityListRequest.new(SD.default_group_id(), Helper.collection_name(), SD.root_key(), query)
+        %Dodai.RetrieveDedicatedDataEntityListSuccess{body: docs} = G2gClient.send(context, SD.app_id(), req)
+        Conn.json(conn, 200, Enum.map(docs, &Helper.to_response_body(&1)))
+    end
+  end
+  defunpt convert_to_dodai_req_query(params :: v[IndexRequestParams.t]) :: Dodai.RetrieveDedicatedDataEntityListRequestQuery.t do
+    query = Map.from_struct(params)
+    |> Enum.reject(fn {_, value} -> is_nil(value) end)
+    |> Enum.map(fn {k, v} -> {"data.#{k}", v} end)
+    |> Map.new()
+
+    %Dodai.RetrieveDedicatedDataEntityListRequestQuery{
+      query: query,
+      sort:  %{"_id" => 1}
     }
-    req = Dodai.RetrieveDedicatedDataEntityListRequest.new(SD.default_group_id(), Helper.collection_name(), SD.root_key(), query)
-    %Dodai.RetrieveDedicatedDataEntityListSuccess{body: docs} = Sazabi.G2gClient.send(conn.context, SD.app_id(), req)
-    Conn.json(conn, 200, Enum.map(docs, &Helper.to_response_body(&1)))
   end
 end
