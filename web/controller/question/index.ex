a69d0a5a1b67b549e09dc3853525bf11e1953d0e
hiroshi.nishigami@access-company.com <Hiroshi.Nishigami@Y55-66745.local>
2019-05-20 10:44:45 +0900

    [add] request parameterとしてuser_idが指定された場合は、その指定された値をもつquestionのみを返すように拡張
    	modified:   web/controller/question/index.ex

--- web/controller/question/index.ex
+++ web/controller/question/index.ex
@@ -4,10 +4,12 @@ defmodule StackoverflowCloneL.Controller.Question.Index do
   alias StackoverflowCloneL.Controller.Question.Helper
 
   def index(conn) do
+    #IO.inspect conn
     query = %Dodai.RetrieveDedicatedDataEntityListRequestQuery{
-      query: %{},
+      query: %{"data.user_id" => conn.request.query_params["user_id"]},
       sort:  %{"_id" => 1},
     }
+    IO.inspect query
     req = Dodai.RetrieveDedicatedDataEntityListRequest.new(SD.default_group_id(), Helper.collection_name(), SD.root_key(), query)
     %Dodai.RetrieveDedicatedDataEntityListSuccess{body: docs} = Sazabi.G2gClient.send(conn.context, SD.app_id(), req)
     Conn.json(conn, 200, Enum.map(docs, &Helper.to_response_body(&1)))
