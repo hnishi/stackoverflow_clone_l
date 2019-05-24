7fbdee34624d8d633ce1a5c4357e6dbef4fc2d3b
aTakatoNakamura <takato.nakamura@access-company.com>
2019-05-24 17:02:04 +0900

    コメントの表示

--- web/static/components/Comment.vue
+++ web/static/components/Comment.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    コメント
+    <!-- !not_implemented! -->
+    <div class="comment">
+      {{ comment.body }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Comment',
+  components: {
+  },
+  props: {
+    comment: {
+      type: Object,
+      required: true,
+    },
+  },
 };
 </script>
 
